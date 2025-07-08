23561cfe1cab96eb0f707b148b372cd5a9b0c2ec
jessicarachel083 <rachel.mld09@gmail.com>
2025-07-08 18:14:39 +0700

    remove import from App.jsx

--- tebak-angka/src/App.jsx
+++ tebak-angka/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import io from "socket.io-client";
-import { useAudio } from "./contexts/AudioContext";
+// import { useAudio } from "./contexts/AudioContext";
 import AudioController from "./components/AudioController";
 import MenuPage from "./pages/MenuPage";
 import GamePage from "./pages/GamePage";
